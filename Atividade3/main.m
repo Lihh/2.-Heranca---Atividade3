a7586106bb15a894e5f72319ada394e435093395
Lidia Chou <lidia.chou94@gmail.com>
2015-02-07 12:15:49 -0200

    Initial Commit

---
+++ Atividade3/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Atividade3
+//
+//  Created by Lidia Chou on 2/7/15.
+//  Copyright (c) 2015 Lidia Chou. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
